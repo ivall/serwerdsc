fbc1fc26a522ff31eb2892e15441765a3c9f9e7d
ivall <ivallpl@gmail.com>
2021-04-12 18:25:29 +0200

    Add config files

---
+++ backend/config.template.py
@@ -0,0 +1,13 @@
+# django config
+DJANGO_SECRET_KEY = ''
+DJANGO_ADMIN_URL = ''
+
+# database config
+DATABASE_NAME = ''
+DATABASE_USER = ''
+DATABASE_PASSWORD = ''
+DATABASE_HOST = ''
+DATABASE_PORT = ''
+
+# global config
+API_TOKEN = ''
